043d9ed9ca475e5a3aac7dfb54af4ed709127df0
blender <coolblender3@gmail.com>
2026-05-12 02:33:46 -0400

    idk

---
+++ src/main/java/blenderscomplex/jar/BlendersComplexClient.java
@@ -0,0 +1,10 @@
+package blenderscomplex.jar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlendersComplexClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
