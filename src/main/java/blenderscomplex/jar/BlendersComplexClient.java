490a69bf8200fdcf7063b538749ea4de29ab0987
blender <coolblender3@gmail.com>
2026-05-14 10:09:33 -0400

    idk

--- src/main/java/blenderscomplex/jar/BlendersComplexClient.java
+++ src/main/java/blenderscomplex/jar/BlendersComplexClient.java
@@ -1,10 +1,13 @@
 package blenderscomplex.jar;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.minecraft.client.util.ModelIdentifier;
 
 public class BlendersComplexClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ModelLoadingRegistry.INSTANCE.registerModelProvider((resources, out) -> out.accept(new ModelIdentifier("blenderscomplex", "flashlight_inhand", "inventory")));
 
     }
 }
